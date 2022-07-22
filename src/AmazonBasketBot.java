4feaa155ea11a6b2cac41ea940ec2eb31723a157
Burak Kurt <burakkurtuk@gmail.com>
2022-07-22 18:36:34 +0300

    topic

--- src/AmazonBasketBot.java
+++ src/AmazonBasketBot.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Amazon {
+public class AmazonBasketBot {
 
     public static void main (String[] args) throws InterruptedException {
 
